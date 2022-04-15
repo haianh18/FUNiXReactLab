8c2198d26640e8a57470483557c7d531bb46dfd6
Hai Anh Lai Vu <laivuhaianh18@gmail.com>
2022-04-15 19:39:48 +0700

    LAB05.3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,18 +6,30 @@ import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from "react-router-dom";
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
+      dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
     }
     
   render() {
     const HomePage = () => {
       return (
-        <Home/>
+        <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       )
     }
     return (
@@ -25,7 +37,9 @@ class Main extends Component {
           <Header/>
           <Switch>
           <Route path='/home' component={HomePage} />
-          <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}></Menu>}/>
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}></Menu>} />
+          <Route exact path='/contactus' component={Contact} />} />
+          <Redirect to='/home'/>
           </Switch>
           <Footer/>
       </div>
