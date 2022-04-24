3aab7f415d20875566638d5feb3ce8bbe7d967b3
Hai Anh Lai Vu <laivuhaianh18@gmail.com>
2022-04-24 19:10:23 +0700

    6th commit Search bar

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,22 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Card, CardText, CardImg } from "reactstrap";
+import { STAFFS } from "../data/staffs";
 
 function StaffList(props) {
   const listNhanvien = props.staffs.map((nv) => {
     return (
-      <Link key={nv.id} to={`/staff/${nv.id}`}>
-        <div key={nv.id}>
-          <div className="col-12 col-md-2 m-1">
-            <img src={nv.image} alt={nv.name} />
-            <p>{nv.name}</p>
-          </div>
-        </div>
-      </Link>
+      <div className="col-6 col-md-4 col-lg-2 mb-1 mt-1">
+        <Card>
+          <Link key={nv.id} to={`/staff/${nv.id}`}>
+            <CardImg
+              className="mx-auto img-thumbnail"
+              src={nv.image}
+              alt={nv.name}
+            />
+            <CardText style={{ color: "black", textAlign: "center" }}>
+              {nv.name}
+            </CardText>
+          </Link>
+        </Card>
+      </div>
     );
   });
+  const [filteredData, setFilteredData] = useState([]);
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    const newFilter = props.staffs.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      return setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="row">{listNhanvien}</div>
+    <div className="search">
+      <div className="searchInputs">
+        <form>
+          <input
+            type="text"
+            placeholder="Tìm nhân viên..."
+            onChange={handleFilter}
+          />
+        </form>
+      </div>
+
+      <div className="dataResult container">
+        <div className="row">
+          {filteredData.map((value) => {
+            return (
+              <div className="col-6 col-md-4 col-lg-2 mb-1 mt-1">
+                <Card>
+                  <Link key={value.id} to={`/staff/${value.id}`}>
+                    <CardImg
+                      className="mx-auto img-thumbnail"
+                      src={value.image}
+                      alt={value.name}
+                    />
+                    <CardText style={{ color: "black", textAlign: "center" }}>
+                      {value.name}
+                    </CardText>
+                  </Link>
+                </Card>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
