b17015c4c0843edced0ee20527fd1de05e83f112
Hai Anh Lai Vu <laivuhaianh18@gmail.com>
2022-04-24 19:33:51 +0700

    7th commit Search Bar

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,74 +1,52 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Card, CardText, CardImg } from "reactstrap";
-import { STAFFS } from "../data/staffs";
 
 function StaffList(props) {
-  const listNhanvien = props.staffs.map((nv) => {
-    return (
-      <div className="col-6 col-md-4 col-lg-2 mb-1 mt-1">
-        <Card>
-          <Link key={nv.id} to={`/staff/${nv.id}`}>
-            <CardImg
-              className="mx-auto img-thumbnail"
-              src={nv.image}
-              alt={nv.name}
-            />
-            <CardText style={{ color: "black", textAlign: "center" }}>
-              {nv.name}
-            </CardText>
-          </Link>
-        </Card>
-      </div>
-    );
-  });
-  const [filteredData, setFilteredData] = useState([]);
-  const handleFilter = (event) => {
-    const searchWord = event.target.value;
-    const newFilter = props.staffs.filter((value) => {
-      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+  const [searchTerm, setSearchTerm] = useState("");
+  const listNhanvien = props.staffs
+    .filter((val) => {
+      if (searchTerm === "") {
+        return val;
+      } else if (
+        val.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      ) {
+        return val;
+      }
+    })
+    .map((nv) => {
+      return (
+        <div className="col-6 col-md-4 col-lg-2 mb-1 mt-1">
+          <Card>
+            <Link key={nv.id} to={`/staff/${nv.id}`}>
+              <CardImg
+                className="mx-auto img-thumbnail"
+                src={nv.image}
+                alt={nv.name}
+              />
+              <CardText style={{ color: "black", textAlign: "center" }}>
+                {nv.name}
+              </CardText>
+            </Link>
+          </Card>
+        </div>
+      );
     });
-
-    if (searchWord === "") {
-      return setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
-  };
 
   return (
     <div className="search">
-      <div className="searchInputs">
-        <form>
-          <input
-            type="text"
-            placeholder="Tìm nhân viên..."
-            onChange={handleFilter}
-          />
-        </form>
+      <div className="searchInputs container m-3">
+        <input
+          type="text"
+          placeholder="Tìm nhân viên..."
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
       </div>
 
-      <div className="dataResult container">
-        <div className="row">
-          {filteredData.map((value) => {
-            return (
-              <div className="col-6 col-md-4 col-lg-2 mb-1 mt-1">
-                <Card>
-                  <Link key={value.id} to={`/staff/${value.id}`}>
-                    <CardImg
-                      className="mx-auto img-thumbnail"
-                      src={value.image}
-                      alt={value.name}
-                    />
-                    <CardText style={{ color: "black", textAlign: "center" }}>
-                      {value.name}
-                    </CardText>
-                  </Link>
-                </Card>
-              </div>
-            );
-          })}
-        </div>
+      <div className="dataResult container m-1">
+        <div className="row">{listNhanvien}</div>
       </div>
     </div>
   );
