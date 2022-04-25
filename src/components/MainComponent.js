2b0cf0fdf1015a1ffe779fde78dd6c2bbac3d662
haianh18 <101784211+haianh18@users.noreply.github.com>
2022-04-25 14:41:21 +0700

    Update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -48,7 +48,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}></Menu>} />
           <Route path="/menu/:dishId" component={DishWithId}/>
-          <Route exact path='/contactus' component={Contact} />} />
+          <Route exact path='/contactus' component={Contact}/>
           <Redirect to='/home'/>
           </Switch>
           <Footer/>
